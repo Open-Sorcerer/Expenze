ee364f9cc71e1840edfb13575b50158c3b273835
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-12-09 00:00:38 +0530

    theming & stores :rainbow:

---
+++ src/components/GroupCard.tsx
@@ -0,0 +1,55 @@
+import Usdt from "icons/Usdt";
+import React from "react";
+import { Pressable, useWindowDimensions } from "react-native";
+import { Box, Text } from "theme";
+import { Group } from "types/common";
+import { useNavigation } from "@react-navigation/native";
+
+type Props = {
+  group: Group;
+};
+
+const GroupCard = ({ group }: Props) => {
+  const { width } = useWindowDimensions();
+  let navigation = useNavigation();
+  return (
+    <Pressable
+      key={group.name}
+      onPress={() => navigation.navigate("ViewGroup")}
+    >
+      <Box
+        flex={1}
+        backgroundColor="mainBackground"
+        width={width * 0.9}
+        flexDirection="row"
+        borderColor={"secondaryCardText"}
+        borderWidth={1}
+        borderRadius={10}
+        p="m"
+        alignItems="center"
+        columnGap={"s"}
+        mt={"s"}
+      >
+        {/*<Box flex={0.15}>{getTokenIcon(group.asset)}</Box>*/}
+        <Box flex={0.15}>{getTokenIcon()}</Box>
+
+        <Box flex={1}>
+          <Text variant="body" color="primaryCardText">
+            {group.name}
+          </Text>
+          <Text color="secondaryCardText">
+            Settled Up: {group.settledUp ? "Yes" : "No"}
+          </Text>
+        </Box>
+      </Box>
+    </Pressable>
+  );
+};
+
+const getTokenIcon = () =>
+  // assetName: string
+  {
+    return <Usdt height={34} width={34} />;
+  };
+
+export default React.memo(GroupCard);
