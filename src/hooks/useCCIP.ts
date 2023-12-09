32478263099f49e2b5f80904bd597bd9b5220d02
Saviour1001 <harshghodkar@gmail.com>
2023-12-09 18:57:52 +0530

    added CCIP hook function

---
+++ src/hooks/useCCIP.ts
@@ -0,0 +1,55 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import { formatUnits } from "viem";
+import {
+  useBlockNumber,
+  useNetwork,
+  useContractWrite,
+  useContractRead,
+  usePublicClient,
+  useWalletClient,
+} from "wagmi";
+import routerABI from "../../cross-chain-contracts/ccip/abi/Router.json";
+import tokenABI from "../../cross-chain-contracts/ccip/abi/IERC20Metadata.json";
+
+const useCCIPTransfer = () => {
+  const client = usePublicClient({
+    chainId: 43_113,
+  });
+
+  const contractAddressChainDB = {
+    EthSepolia: "0xD0daae2231E9CB96b94C8512223533293C3693Bf",
+    OptimismGoerli: "0xEB52E9Ae4A9Fb37172978642d4C141ef53876f26",
+    AvalancheFuji: "0x554472a2720E5E7D5D3C817529aBA05EEd5F82D8",
+    ArbitrumTestnet: "0x88E492127709447A5ABEFdaB8788a15B4567589E",
+    PolygonMumbai: "0x70499c328e1E2a3c41108bd3730F6670a44595D1",
+  };
+
+  const destinationChainIDDB = {
+    EthSepolia: "16015286601757825753",
+    OptimismGoerli: "2664363617261496610",
+    AvalancheFuji: "14767482510784806043",
+    ArbitrumTestnet: "6101244977088475029",
+    PolygonMumbai: "12532609583862916517",
+  };
+
+  const SendBlockchainTxn = async (
+    destinationChainID: string,
+    destinationAddress: string,
+    amount: number
+  ) => {
+    const { data, writeAsync } = useContractWrite({
+      address: "0x554472a2720E5E7D5D3C817529aBA05EEd5F82D8",
+      abi: routerABI,
+      functionName: "ccipSend",
+    });
+
+    // todo - add args
+
+    const tx = await writeAsync({
+      args: [],
+    });
+  };
+};
+
+export default useCCIPTransfer;
